4f65e2318fdda89663dfcbfa109d6a3f1e24bb1a
Siroj003 <tuxtamurodovsiroj9@gmail.com>
2024-10-24 12:36:45 -0500

    init

---
+++ data/index.ts
@@ -0,0 +1,203 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for deployment.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building ELD",
+    description: "Logictics app",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Martian logistics site deployment",
+    des: "Explore martian.com",
+    img: "/martian.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/martian.uz",
+  },
+  {
+    id: 2,
+    title: "ELD logistics app deployment",
+    des: "Explore zeld",
+    img: "/eld.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/eld.com",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Siroj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Siroj's enthusiasm for every facet of deployment truly stands out. If you're seeking to elevate your website and elevate your brand, Siroj is the ideal partner.",
+    name: "Ismoil Mirabosov",
+    title: "Senior devops",
+  },
+  {
+    quote:
+      "Collaborating with Siroj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Siroj's enthusiasm for every facet of deployment truly stands out. If you're seeking to elevate your website and elevate your brand, Siroj is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Siroj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Siroj's enthusiasm for every facet of deployment truly stands out. If you're seeking to elevate your website and elevate your brand, Siroj is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Siroj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Siroj's enthusiasm for every facet of deployment truly stands out. If you're seeking to elevate your website and elevate your brand, Siroj is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Siroj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Siroj's enthusiasm for every facet of deployment truly stands out. If you're seeking to elevate your website and elevate your brand, Siroj is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Devops Intern",
+    desc: "Assisted in the deployment, enhancing security.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Devops engineer",
+    desc: "Enhanced security and deployed using tools like kubernetes, docker and terraform",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Devops projects",
+    desc: "Led the devops of a web apps for a client, from initial concept to deployment on clouds.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Devops",
+    desc: "Automated processes via ci/cd. Deployment to aws clouds",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/Siroj003"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://twitter.com"
+  },
+  {
+    id: 3,
+    img: "/telegram.svg",
+    url: "https://t.me/siroj9656"
+  },
+];
